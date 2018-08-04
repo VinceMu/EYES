081ba2fff417da6785b24f573a08897bd4475a78
jeremytay90 <jeremytay90@gmail.com>
2018-08-05 03:03:31 +1000

    Fix post data

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,15 +1,31 @@
-  let changeColor = document.getElementById('changeColor');
+  //let changeColor = document.getElementById('changeColor');
+  let toggleOn = document.getElementById('toggleOn');
+  let toggleCam = document.getElementById('toggleCamera');
 
-  chrome.storage.sync.get('color', function(data) {
+  console.log('hi');
+  toggleOn.addEventListener('change',function () {
+  	if(this.checked) {
+  		chrome.storage.sync.set({gazeOn: true}, function() {
+     		 console.log('Turned on');
+    	})
+  	} else {
+  		chrome.storage.sync.set({gazeOn: false}, function() {
+     		 console.log('Turned off');
+    	})
+  	}
+    
+  });
+
+  /*chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
   });
 
-	changeColor.onclick = function(element) {
+  changeColor.onclick = function(element) {
     let color = element.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
-  };
+  };*/
