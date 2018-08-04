2d62892f3626bc14e1d23bb7be37e491497dbf7c
Vincent Tsai <vincentvtt@hotmail.com>
2018-08-04 17:06:00 +1000

    processor

---
+++ processor/processor.py
@@ -0,0 +1,27 @@
+import threading
+import os
+import ssl
+from pymongo import MongoClient
+
+os.environ["TIMERINTERVAL"] = "0"
+uri = "mongodb+srv://user:123@unihack-dt2qp.mongodb.net/test?authMechanism=SCRAM-SHA-1"
+client = MongoClient(uri, ssl_cert_reqs=ssl.CERT_NONE)
+print("here")
+
+mydb = client["UNIHACK"]
+mycol = mydb["eye-data"]
+
+myquery = { "test" : "data" }
+mydoc = mycol.find(myquery)
+
+for x in mydoc:
+    print(x)
+
+def repeatloop():
+
+    # class threading.Timer(interval, function, args=None, kwargs=None)
+    threading.Timer(5.0, repeatloop).start()
+    print(os.environ["TIMERINTERVAL"])
+
+
+repeatloop()
