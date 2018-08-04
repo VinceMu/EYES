4ce97c1008758357854ca2725a70d595a6d51e8c
Vincent Tsai <vincentvtt@hotmail.com>
2018-08-04 21:00:47 +1000

    mergin

--- clashboard-backend/app.py
+++ clashboard-backend/app.py
@@ -2,26 +2,41 @@ from pymongo import MongoClient
 from flask import request, jsonify
 from flask import Flask
 from bson.json_util import dumps
-
 import ssl
+import json
+from pprint import pprint
+
 
 uri = "mongodb+srv://user:123@unihack-dt2qp.mongodb.net/test?authMechanism=SCRAM-SHA-1"
 client = MongoClient(uri, ssl_cert_reqs=ssl.CERT_NONE)
 
 app = Flask(__name__)
+mydb = client["UNIHACK"]
+mycol = mydb["eye-data"]
 
 
-@app.route('/getData', methods=['GET', 'POST'])
-def user():
-    mydb = client["UNIHACK"]
-    mycol = mydb["eye-data"]
+@app.route('/getData', methods=['GET'])
+def getFromMongo():
+
     if request.method == 'GET':
         myquery = {"website": "www.firsttest.com"}
         mydoc = mycol.find_one(myquery)
         return dumps(mydoc)
 
-@app.route("/postData",methods=["POST"])
+
+@app.route("/postData", methods=["POST"])
 def insertToMongo():
+    json_data = request.get_data()
+    d_list = json.loads(json_data)
+
+    insert = {
+        "website": d_list['website'],
+        "data": d_list['data'],
+    }
+    print(insert)
+
+    mycol.insert_one(insert)
+
     print("recieved post")
     return "OK"
 
